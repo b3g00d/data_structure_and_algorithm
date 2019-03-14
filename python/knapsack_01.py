e644afb6ab0e331e8dc33c1314c9638001c2ce13
quangvn <mr2uang55@gmail.com>
2019-03-14 13:41:57 +0700

    Add: format file

--- python/knapsack_01.py
+++ python/knapsack_01.py
@@ -3,22 +3,21 @@ def init_knapsack():
 
 
 def find_solution_knapsack(w, v, W):
-    solutions = [[None for _ in range(W+1)] for _ in range(len(w)+1)]
+    solutions = [[None for _ in range(W + 1)] for _ in range(len(w) + 1)]
 
-    for i in range(W+1):
+    for i in range(W + 1):
         solutions[0][i] = 0
 
-    for i in range(1, len(w)+1):
-        for j in range(W+1):
-            if w[i-1] <= j:
-                solutions[i][j] = max(solutions[i - 1][j],
-                                      v[i-1] + solutions[i - 1][j - w[i-1]])
+    for i in range(1, len(w) + 1):
+        for j in range(W + 1):
+            if w[i - 1] <= j:
+                solutions[i][j] = max(
+                    solutions[i - 1][j],
+                    v[i - 1] + solutions[i - 1][j - w[i - 1]])
             else:
-                solutions[i][j] = solutions[i-1][j]
+                solutions[i][j] = solutions[i - 1][j]
     for i in solutions:
         print(i)
-
-
 
 
 if __name__ == "__main__":
